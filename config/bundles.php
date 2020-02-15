b4f4798531ad6494402d95fe974fddec912c70d7
SametYILMAZ <sametyilmazx@gmail.com>
2020-02-16 00:52:36 +0300

    FOS User, Doctrine, ORM, Security, Twig, Form, Translation, Profiler, PHPUnit, SwiftMailer and some dependency added and configurations.

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,10 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     WorkBundles\WorkHubElasticaBundle\WorkHubElasticaBundle::class => ['all' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
+    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
