3f3cdaf2fbd8a729eec925d2d6f2a1a34769a55e
badgeekluck <harun.1996.187@gmail.com>
2020-05-08 00:07:27 +0300

    user entity and some fixes

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,8 +10,19 @@ namespace WorkHub\Entity;
 
 
 use FOS\UserBundle\Model\User as FOSUser;
+use Doctrine\ORM\Mapping as ORM;
 
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="User")
+ */
 class User extends FOSUser
 {
-
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer", name="id")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
 }
